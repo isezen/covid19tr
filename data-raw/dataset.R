bc83b8c7fd0d142f92450a9dc7dc5289a82741f9
Ismail SEZEN <613356+isezen@users.noreply.github.com>
2020-04-29 14:12:32 +0300

    update

--- data-raw/dataset.R
+++ data-raw/dataset.R
@@ -1,17 +1,16 @@
 library(usethis)
 
-cn <- c("date", "country_region", "province_state", "type",
-        "cases", "lat", "long")
+cn <- c("date", "country_region", "province_state", "type", "cases")
 filename <- 'data-raw/c19tr.csv'
 c19tr <- read.csv2(filename, na.strings = "",
                    colClasses = c("Date", "integer", "integer",
                                 "integer", "integer", "integer", "integer"))
+c19tr$recovered <- as.integer(zoo::na.approx(c19tr$recovered))
+c19tr$active <- with(c19tr, confirmed - deaths - recovered)
 c19trl <- reshape2::melt(c19tr, id.vars = c("date"), variable.name = "type", value.name = "cases")
 c19trl$type <- as.character(c19trl$type)
 c19trl$country_region <- "Turkey"
 c19trl$province_state <- NA
-c19trl$lat <- 38.9637
-c19trl$long <- 35.2433
 c19trl <- c19trl[,cn]
 c19trl <- c19trl[order(c19trl$date, c19trl$type),]
 c19trl$country_region <- factor(c19trl$country_region)
